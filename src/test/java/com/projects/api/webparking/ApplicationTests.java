58497f7da4f16d552e79da4d7321943d6c8ba7e7
Jonathan Emanuel <jonathan.tolentino@clicksoft.com.br>
2023-07-22 11:04:41 -0300

    initial commit

---
+++ src/test/java/com/projects/api/webparking/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.api.webparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
